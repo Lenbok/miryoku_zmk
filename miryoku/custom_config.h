32969159ed95a0a3773d9b57aa50731bab2fee95
Lenbok <lenbok@gmail.com>
2022-06-15 11:03:09 +1200

    Instead of u_out_tog, use u_bt_sel_4

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,5 +2,12 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS_QWERTY
-//#define MIRYOKU_NAV_VI
+
 #define MIRYOKU_LAYERS_FLIP
+
+// Swap u_out_tog for u_bt_sel_4
+#define MIRYOKU_LAYER_MEDIA \
+U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         &rgb_ug RGB_TOG,   U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
+&kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_bt_sel_4,       U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
+U_NP,              U_NP,              &kp C_MUTE,        &kp C_PP,          &kp C_STOP,        U_NA,              U_NA,              U_NA,              U_NP,              U_NP
